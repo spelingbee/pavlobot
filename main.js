50efdc1d18d2d992cbd38be21903d2da18c1cc60
Тимур <kadyrbekov99@inbox.ru>
2023-10-08 19:55:09 +0600

    test

--- main.js
+++ main.js
@@ -2,16 +2,63 @@ import axios from "axios";
 import * as dotenv from 'dotenv'
 import http from 'http'
 dotenv.config()
-setInterval(async () => {
-    try {
-        await axios({
-            method: "POST",
-            url: process.env.API_URL + '/checkouts/check'
-        })
-    } catch (e) {
-        console.log(e)
+import WebSocket from "ws";
+const websocketUrl = "wss://ringotrade.com/ws";
+const socket = new WebSocket(websocketUrl);
+
+socket.addEventListener("open", (event) => {
+    console.log("Соединение установлено");
+});
+const first = [false, false];
+socket.addEventListener("message", (event) => {
+    const data = JSON.parse(event.data).data
+    const type = JSON.parse(event.data).type
+    let fetchData = null
+    let url = ''
+    if (type === 'last-results') {
+        fetchData = JSON.stringify(data.map(item => {
+            return {
+                value: item.v,
+                position: item.c,
+                createdDate: item.dt
+            }
+        }))
+    } else if (type === 'factors') {
+        if (first[0]) {
+            fetchData = JSON.stringify(data.at(-1))
+
+        } else {
+            fetchData = JSON.stringify(data)
+            first[0] = true
+        }
+        url = '/socket-factor'
+    } else {
+        fetchData = JSON.stringify(data)
+        url = '/socket-round'
     }
-}, 2000)
+    url = `https://ringo-inky.vercel.app/ringo${url}`
+    fetch(url, {
+        method: 'POST',
+        body: fetchData,
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+});
+
+socket.addEventListener("close", (event) => {
+    if (event.wasClean) {
+        console.log(
+            `Соединение закрыто чисто, код: ${event.code}, причина: ${event.reason}`
+        );
+    } else {
+        console.error("Соединение прервано");
+    }
+});
+
+socket.addEventListener("error", (error) => {
+    console.error("Произошла ошибка:", error);
+});
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
